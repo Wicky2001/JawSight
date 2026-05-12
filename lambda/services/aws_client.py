3ef53afdb7d14d07c8493dea9cdac6411584df92
Pasindu Wickramaratna <pmwickramaratna@gmail.com>
2026-05-12 16:02:41 +0530

    remove manual profile setting up

--- lambda/services/aws_client.py
+++ lambda/services/aws_client.py
@@ -7,13 +7,8 @@ _session = None
 def get_boto_session():
     global _session
     if _session is None:
-        logger.info("Checking AWS session... environment=%s", ENV)
-        if ENV == 'development':
-            logger.info("✅ SUCCESS: Setting up custom AWS session for development using profile '%s'", AWS_PROFILE)
-            _session = boto3.Session(profile_name=AWS_PROFILE)
-        else:
-            logger.info("✅ SUCCESS: Using default implicit AWS session (production).")
-            _session = boto3.Session()
+        logger.info("✅ SUCCESS: Using default implicit AWS session (production).")
+        _session = boto3.Session()
     return _session
 
 s3_client = get_boto_session().client('s3')
